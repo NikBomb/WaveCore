f49b24731873d4fbf6069af1612e46fc5a391496
Nico Bombace <nico.bombace@amd.com>
2026-05-07 17:07:30 +0100

    Add init to systems

---
+++ wavecore/systems/__init__.py
@@ -0,0 +1,17 @@
+from .kinematics import (update_element_density, 
+                         update_element_jacobian, 
+                         update_element_length, 
+                         update_element_kinematics, 
+                         update_node_current_position, 
+                         update_element_rate_of_deformation)
+
+__all__ = [
+    # Kinematics 
+                         update_element_density, 
+                         update_element_jacobian, 
+                         update_element_length, 
+                         update_element_kinematics, 
+                         update_node_current_position, 
+                         update_element_rate_of_deformation
+    #Timestep
+]
